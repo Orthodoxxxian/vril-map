0eb15e19cc1a9585a1d81666fe0108924f48f86f
Orthodoxxxian <dushkin16062000@gmail.com>
2025-06-22 21:50:42 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+fetch("data.json").then(r=>r.json()).then(points=>{
+  const map = L.map("map").setView([50,30],4);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+  const cluster = L.markerClusterGroup();
+  points.forEach(p=>{
+    const icon = L.divIcon({
+      html:`<img class="rotating" src="icon.png" style="transform:rotate(${p.vril}deg)" />`,
+      className:"vril-icon", iconSize:[40,40]
+    });
+    const m = L.marker([p.lat,p.lon],{icon});
+    m.bindPopup(`<b>${p.city}, ${p.country}</b><br>
+      Вриль: ${p.vril.toFixed(1)}<br>
+      ${p.desc}, ${p.temp}°C`);
+    cluster.addLayer(m);
+  });
+  map.addLayer(cluster);
+});
